46ca842eaac69ddb386a4483600ba030a20e97df
Tarek Radwan <tarek.radwan.94@gmail.com>
2021-09-04 10:53:46 +0200

    Footer implementation

--- src/components/containers/RMFooterContainer.js
+++ src/components/containers/RMFooterContainer.js
@@ -1,7 +1,11 @@
+import './RMFooterContainer.css';
+
 function RMFooterContainer() {
     return (
         <div className="RMFooterContainer">
-
+            <div className="RMFooterDisclaimerContainer">
+                <span>Cloud Academy assignment</span><br /><span>Developed by T. Radwan</span>
+            </div>
         </div>
     );
 }
